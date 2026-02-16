77c867610f37c1085f0f721b0d4fc6a868a37298
rafaelseverom <rafasevero.m@gmail.com>
2026-02-16 14:19:47 -0300

    chore: atualizar estrutura do projeto e registrar routers

--- app/routers/projetos.py
+++ app/routers/projetos.py
@@ -1,11 +1,17 @@
 # ATUALIZAR ARQUIVO DE ROTAS
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Query
 from sqlalchemy.orm import Session
+from sqlalchemy import asc, desc
+
 from app.database import SessionLocal
 from app.models.projeto import Projeto
+from app.models.usuario import Usuario
 from app.schemas.projeto import ProjetoCreate, ProjetoUpdate, ProjetoOut
 from app.core.security import get_usuario_logado
-from app.models.usuario import Usuario
+from app.core.enums import StatusProjeto
+from typing import Optional
+
+
 
 
 router = APIRouter(prefix="/projetos", tags=["Projetos"])
@@ -16,6 +22,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
 @router.post("/", response_model=ProjetoOut)
 def criar_projeto(
     projeto: ProjetoCreate,
@@ -34,16 +41,53 @@ def criar_projeto(
     db.refresh(novo_projeto)
     return novo_projeto
 
-
-
 @router.get("/")
 def listar_projetos(
+    pagina: int = Query(1, ge=1),
+    limite: int = Query(10, ge=1, le=100),
+    ordem: str = Query("desc"),
+    status: Optional[StatusProjeto] = Query(None),
     usuario: Usuario = Depends(get_usuario_logado),
     db: Session = Depends(get_db)
 ):
-    return db.query(Projeto).filter(
-    Projeto.usuario_id == usuario.id
-).all()
+    query = db.query(Projeto).filter(
+        Projeto.usuario_id == usuario.id
+    )
+
+    # FILTRO POR STATUS
+    if status:
+        query = query.filter(Projeto.status == status)
+
+    # ORDENAÇÃO
+    if ordem == "desc":
+        query = query.order_by(desc(Projeto.data_criacao))
+    else:
+        query = query.order_by(asc(Projeto.data_criacao))
+
+    # PAGINAÇÃO
+    offset = (pagina - 1) * limite
+    projetos = query.offset(offset).limit(limite).all()
+
+    return projetos
+
+
+
+@router.get("/{projeto_id}", response_model=ProjetoOut)
+def obter_projeto(
+    projeto_id: int,
+    usuario: Usuario = Depends(get_usuario_logado),
+    db: Session = Depends(get_db)
+):
+    projeto = db.query(Projeto).filter(Projeto.id == projeto_id).first()
+
+    if not projeto:
+        raise HTTPException(status_code=404, detail="Projeto não encontrado")
+
+    # segurança → só dono pode ver
+    if projeto.usuario_id != usuario.id:
+        raise HTTPException(status_code=403, detail="Acesso negado")
+
+    return projeto
 
 
 
